f0254728d430c727a3b7ccfc1976ea5015b6566a
abdelhamed630 <abdelhmedmostafe@gmail.com>
2026-03-12 00:16:45 +0200

    Add vercel and proxy config

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': { target: 'http://localhost:8000', changeOrigin: true },
-      '/media': { target: 'http://localhost:8000', changeOrigin: true }
+      '/api': { target: 'https://elearing-production-2841.up.railway.app', changeOrigin: true },
+      '/media': { target: 'https://elearing-production-2841.up.railway.app', changeOrigin: true }
     }
   },
   build: {
